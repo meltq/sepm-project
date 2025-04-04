d28c7c7fb0684e9411839f51da265dcdf024de49
meltq <alissxlace@proton.me>
2025-04-04 10:25:13 +0530

    Finished cleaning fr

--- cleaning/2-only-deps.py
+++ cleaning/2-only-deps.py
@@ -7,22 +7,19 @@ ls = os.listdir(home+"jsons")
 files = [f for f in ls if os.path.isfile(home+'jsons/'+f)]
 
 for i in files:
-    try:
-        with open(home+'jsons/'+i, "r") as file:
-            data = json.load(file)
-        
-        deps = data["deps"]
-        dep_names = []
-        for j in deps:
-            dep_names.append(j['name'])
-        to_dump = {
-            "name": data["name"],
-            "deps": dep_names,
-        }
-        print(to_dump)
+    with open(home+'jsons/'+i, "r") as file:
+        data = json.load(file)
+    
+    deps = data["deps"]
+    dep_names = []
+    for j in deps:
+        dep_names.append(j['name'])
+    to_dump = {
+        "name": data["name"],
+        "deps": dep_names,
+    }
+    print(to_dump)
 
-        with open(home+'jsons/cleaned/'+i, "w") as file:
-            json.dump(data, file, indent=4)
-        print(i, "updated successfully!")
-    except:
-        pass
+    with open(home+'jsons/cleaned/'+i, "w") as file:
+        json.dump(to_dump, file, indent=4)
+    print(i, "updated successfully!")
